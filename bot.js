4af815fc086b37c2031a67117812f8e2c2ce78ae
aaaymen <45045194+aaaymen@users.noreply.github.com>
2019-01-11 01:51:40 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,6 +1,9 @@
 ﻿const Discord = require('discord.js');
 const client = new Discord.Client();
 
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);  
+});
 
  
  client.on('guildMemberRemove', member => {
